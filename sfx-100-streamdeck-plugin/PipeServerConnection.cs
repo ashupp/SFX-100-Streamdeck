95424f4816c913a78b022a4530b260d9a5a9ee61
Alexander Hupp <ahupp@ashnet.de>
2020-04-19 12:09:07 +0200

    Better connectivity between Streamdeck and Simfeedback

--- sfx-100-streamdeck-plugin/PipeServerConnection.cs
+++ sfx-100-streamdeck-plugin/PipeServerConnection.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using BarRaider.SdTools;
 
 namespace sfx_100_streamdeck_plugin
 {
@@ -13,13 +14,35 @@ namespace sfx_100_streamdeck_plugin
         private static volatile PipeServerConnection instance;
         private static object syncRoot = new Object();
         public PipeContract Channel;
+        private NetNamedPipeBinding _binding;
+        private EndpointAddress _ep ;
 
         private PipeServerConnection()
         {
+            createServer();
+        }
+
+        void createServer()
+        {
+            Logger.Instance.LogMessage(TracingLevel.INFO, "createServer");
             string address = "net.pipe://localhost/ashnet/StreamDeckExtension";
-            NetNamedPipeBinding binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
-            EndpointAddress ep = new EndpointAddress(address);
-            Channel = ChannelFactory<PipeContract>.CreateChannel(binding, ep);
+            _binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+            _ep = new EndpointAddress(address);
+            Channel = ChannelFactory<PipeContract>.CreateChannel(_binding, _ep);
+
+            ((ICommunicationObject)Channel).Faulted += ProxyServiceFactory_Faulted;
+        }
+
+
+        void ProxyServiceFactory_Faulted(object sender, EventArgs e)
+        {
+            Logger.Instance.LogMessage(TracingLevel.INFO, "Channel Faulted...");
+            ((ICommunicationObject)sender).Abort();
+            if (sender is ChannelFactory<PipeContract>)
+            {
+                Logger.Instance.LogMessage(TracingLevel.INFO, "Recreating...");
+                Channel = ChannelFactory<PipeContract>.CreateChannel(_binding, _ep);
+            }
         }
 
         public static PipeServerConnection Instance
