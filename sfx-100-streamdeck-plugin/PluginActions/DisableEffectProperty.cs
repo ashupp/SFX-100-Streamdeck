bbeadc7427da2dbc74a83e276f208dcf959f4ffc
Alexander Hupp <ahupp@ashnet.de>
2020-05-05 10:26:43 +0200

    New Stream Deck Buttons "Enable / Disable Effect Property"
    - Version Bump 0.0.2.2

---
+++ sfx-100-streamdeck-plugin/PluginActions/DisableEffectProperty.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading.Tasks;
+using BarRaider.SdTools;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.ServiceModel;
+
+
+namespace sfx_100_streamdeck_plugin.PluginActions
+{
+    [PluginActionId("sfx-100-streamdeck-plugin.disableeffectproperty")]
+    public class DisableEffectProperty : PluginBase
+    {
+        private class PluginSettings
+        {
+            public static PluginSettings CreateDefaultSettings()
+            {
+                PluginSettings instance = new PluginSettings();
+                instance.EffectName = "Heave";
+                instance.ValueToChange = "Enable";
+                return instance;
+            }
+
+            [JsonProperty(PropertyName = "EffectName")]
+            public string EffectName { get; set; }
+
+            [JsonProperty(PropertyName = "ValueToChange")]
+            public string ValueToChange { get; set; }
+
+        }
+
+        #region Private Members
+
+        private PluginSettings settings;
+
+        #endregion
+        public DisableEffectProperty(SDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+            if (payload.Settings == null || payload.Settings.Count == 0)
+            {
+                settings = PluginSettings.CreateDefaultSettings();
+            }
+            else
+            {
+                settings = payload.Settings.ToObject<PluginSettings>();
+            }
+        }
+
+        public override void Dispose() { }
+
+        public override void KeyPressed(KeyPayload payload) { }
+
+        public override void KeyReleased(KeyPayload payload)
+        {
+            try
+            {
+                PipeServerConnection.Instance.RestartChannel();
+                switch (settings.ValueToChange)
+                {
+                    case "Linear":
+                        PipeServerConnection.Instance.Channel.EffectLinearDisable(settings.EffectName);
+                        break;
+                    case "Enable":
+                        PipeServerConnection.Instance.Channel.EffectDisable(settings.EffectName);
+                        break;
+                    case "Mute":
+                        PipeServerConnection.Instance.Channel.EffectMuteDisable(settings.EffectName);
+                        break;
+                    default:
+                        Logger.Instance.LogMessage(TracingLevel.ERROR, "Error: ValueToChange not set correctly: " + settings.ValueToChange);
+                        break;
+                }
+            }
+            catch (EndpointNotFoundException endpointNotFoundException)
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, "Error: Endpoint not found - Is SimFeedback available and is the Plugin enabled? " + endpointNotFoundException.Message);
+            }
+            catch (CommunicationObjectFaultedException communicationObjectFaultedException)
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, "Error: communicationObjectFaultedException: " + communicationObjectFaultedException.Message);
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, "Error during Key processing: " + ex.Message);
+            }
+        }
+
+        public override void OnTick() { }
+
+        public override void ReceivedSettings(ReceivedSettingsPayload payload)
+        {
+            Tools.AutoPopulateSettings(settings, payload.Settings);
+            SaveSettings();
+        }
+
+        public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload) { }
+
+        #region Private Methods
+
+        private Task SaveSettings()
+        {
+            return Connection.SetSettingsAsync(JObject.FromObject(settings));
+        }
+
+        #endregion
+    }
+}
