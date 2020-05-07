1fb804858a37e87655c95e722ba63a125cec4a24
Alexander Hupp <ahupp@ashnet.de>
2020-05-07 22:21:05 +0200

    Added Buttons and sorted existing buttons
    Temporary remove "start minimized" since it is incompatible with newest functions

---
+++ sfx-100-streamdeck-plugin/PluginActions/SetOverallIntensityValue.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading.Tasks;
+using BarRaider.SdTools;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.ServiceModel;
+
+
+namespace sfx_100_streamdeck_plugin.PluginActions
+{
+    [PluginActionId("sfx-100-streamdeck-plugin.setoverallintensityvalue")]
+    public class SetOverallIntensityValue : PluginBase
+    {
+        private class PluginSettings
+        {
+            public static PluginSettings CreateDefaultSettings()
+            {
+                PluginSettings instance = new PluginSettings();
+                instance.Value = 50;
+                return instance;
+            }
+
+            [JsonProperty(PropertyName = "Value")]
+            public int Value { get; set; }
+
+        }
+
+        #region Private Members
+
+        private PluginSettings settings;
+
+        #endregion
+        public SetOverallIntensityValue(SDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+            if (payload.Settings == null || payload.Settings.Count == 0)
+            {
+                settings = PluginSettings.CreateDefaultSettings();
+                SaveSettings();
+            }
+            else
+            {
+                settings = payload.Settings.ToObject<PluginSettings>();
+            }
+        }
+
+        public override void Dispose() { }
+
+        public override void KeyPressed(KeyPayload payload) { }
+
+        public override void KeyReleased(KeyPayload payload)
+        {
+            try
+            {
+                PipeServerConnection.Instance.RestartChannel();
+                PipeServerConnection.Instance.Channel.SetOverallIntensity(settings.Value);
+            }
+            catch (EndpointNotFoundException endpointNotFoundException)
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, "Error: Endpoint not found - Is SimFeedback available and is the Plugin enabled? " + endpointNotFoundException.Message);
+            }
+            catch (CommunicationObjectFaultedException communicationObjectFaultedException)
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, "Error: communicationObjectFaultedException: " + communicationObjectFaultedException.Message);
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.LogMessage(TracingLevel.ERROR, "Error during Key processing: " + ex.Message);
+            }
+        }
+
+        public override void OnTick() { }
+
+        public override void ReceivedSettings(ReceivedSettingsPayload payload)
+        {
+            Tools.AutoPopulateSettings(settings, payload.Settings);
+            SaveSettings();
+        }
+
+        public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload) { }
+
+        #region Private Methods
+
+        private Task SaveSettings()
+        {
+            return Connection.SetSettingsAsync(JObject.FromObject(settings));
+        }
+
+        #endregion
+    }
+}
