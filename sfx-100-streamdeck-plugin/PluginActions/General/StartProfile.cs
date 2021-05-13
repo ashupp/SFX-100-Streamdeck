982e9f4c75eee7dcfb4ed683f355e4cd38d39224
dl <dl@DESKTOP-AJBAKU1>
2021-05-13 18:00:23 +0200

    Added extraction of path for profile so that there are different profile directories are possible (speedup)

--- sfx-100-streamdeck-plugin/PluginActions/General/StartProfile.cs
+++ sfx-100-streamdeck-plugin/PluginActions/General/StartProfile.cs
@@ -172,7 +172,13 @@ namespace sfx_100_streamdeck_plugin.PluginActions
                 var windowStyle = (ProcessWindowStyle) Enum.Parse(typeof(ProcessWindowStyle), settings.SfbWindowStyle);
                 psi.WindowStyle = windowStyle;
             }
-            
+            psi.Arguments = "-p " + Path.GetDirectoryName(settings.ProfileToLoad);
+
+            psi.Arguments = "-p profiles.smp";
+
+            psi.Arguments  = "-p " +  new DirectoryInfo(settings.ProfileToLoad).Parent.Name;
+
+            Logger.Instance.LogMessage(TracingLevel.DEBUG, "Arguments " + psi.Arguments);
             psi.WorkingDirectory = Path.GetDirectoryName(settings.SfbExe);
             var newProc = Process.Start(psi);
             newProc.WaitForInputIdle();
